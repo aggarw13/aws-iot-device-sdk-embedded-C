8c0cdb6d281af076e79a957d45c955bcaba68645
Archit Aggarwal <architag@amazon.com>
2020-05-14 17:50:25 +0000

    Update Approach_1/app_1 to use C89 way of adding metadata

--- approach_1/app_1/mqtt_config.h
+++ approach_1/app_1/mqtt_config.h
@@ -1,18 +1,14 @@
 #ifndef __MQTT_CONFIG_H__
 #define __MQTT_CONFIG_H__
 
-#include "c99_logging_setup.h"
+#include <stdio.h>
 
-#define LIBRARY_LOG_NAME    "MQTT"
+#define CommonLog( message )    printf message
 
-#define LogError( message )                     Log( LIBRARY_LOG_NAME, LOG_ERROR, message )
-#define LogErrorWithArgs( formatAndStrings )    LogWithArgs( LIBRARY_LOG_NAME, LOG_ERROR, formatAndStrings )
-#define LogWarn( message )                      Log( LIBRARY_LOG_NAME, LOG_WARN, message )
-#define LogWarnWithArgs( formatAndStrings )     LogWithArgs( LIBRARY_LOG_NAME, LOG_WARN, formatAndStrings )
-#define LogInfo( message )                      Log( LIBRARY_LOG_NAME, LOG_INFO, message )
-#define LogInfoWithArgs( formatAndStrings )     LogWithArgs( LIBRARY_LOG_NAME, LOG_INFO, formatAndStrings )
-#define LogDebug( message )                     Log( LIBRARY_LOG_NAME, LOG_DEBUG, message )
-#define LogDebugWithArgs( formatAndStrings )    LogWithArgs( LIBRARY_LOG_NAME, LOG_DEBUG, formatAndStrings )
+#define LogError( message )     CommonLog( ( "[ERROR] [MQTT] [%s] [%s]", __FILE__, __LINE__ ) ); CommonLog( message );
+#define LogWarn( message )      CommonLog( ( "[WARN] [MQTT] [%s] [%s]", __FILE__, __LINE__ ) ); CommonLog( message );
+#define LogInfo( message )      CommonLog( ( "[INFO] [MQTT] [%s] [%s]", __FILE__, __LINE__ ) ); CommonLog( message );
+#define LogDebug( message )     CommonLog( ( "[DEBUG] [MQTT] [%s] [%s]", __FILE__, __LINE__ ) ); CommonLog( message );
 
 
 #endif /* ifndef __MQTT_CONFIG_H__ */
--- approach_1/mqtt/mqtt_client.c
+++ approach_1/mqtt/mqtt_client.c
@@ -4,11 +4,11 @@
 void MQTT_PublishMessage( void )
 {
     LogError( ( "Error log string.\n" ) );
-    LogErrorWithArgs( ( "Error log with one arg: %d.\n", 1 ) );
+    LogError( ( "Error log with one arg: %d.\n", 1 ) );
     LogWarn( ( "Warning log string.\n" ) );
-    LogWarnWithArgs( ( "Warning log with one arg: %d.\n", 1 ) );
+    LogWarn( ( "Warning log with one arg: %d.\n", 1 ) );
     LogInfo( ( "Info log string.\n" ) );
-    LogInfoWithArgs( ( "Info log with one arg: %d.\n", 1 ) );
+    LogInfo( ( "Info log with one arg: %d.\n", 1 ) );
     LogDebug( ( "Debug log string.\n" ) );
-    LogDebugWithArgs( ( "Debug log with one arg: %d.\n", 1 ) );
+    LogDebug( ( "Debug log with one arg: %d.\n", 1 ) );
 }
